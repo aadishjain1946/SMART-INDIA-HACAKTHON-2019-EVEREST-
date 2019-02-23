f91c728116b3af2a9b8962850e5ebdd26b75856a
Aadish Jain <aadishjain1946@gmail.com>
2019-02-23 19:58:16 +0530

    Add files via upload

--- EVEREST/src/login/logincontroller.js
+++ EVEREST/src/login/logincontroller.js
@@ -1,10 +1,29 @@
 window.addEventListener("load", init);
 function init() {
     bindevents();
+    document.querySelector("#submit").setAttribute("cust","u");
+
 }
-function bindvents(){
+function bindevents(){
     document.querySelector("#loginid").addEventListener("blur",chceckloginID);
+    document.querySelector("#Admin").addEventListener("click",Admin);
+    document.querySelector("#User").addEventListener("click",User);
 }
 function chceckloginID(){
     var id = document.querySelector("#loginid").Value;
+}
+function User(){
+    document.querySelector("#submit").setAttribute("cust","u");
+    var User = document.querySelector("#User");
+    var Admin = document.querySelector("#Admin");
+    Admin.classList.remove('active');
+    User.classList.add('active');
+
+}
+function Admin(){
+    document.querySelector("#submit").setAttribute("cust","a");
+    var User = document.querySelector("#User");
+    var Admin = document.querySelector("#Admin");
+    User.classList.remove('active');
+    Admin.classList.add('active');
 }
